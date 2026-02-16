119e58bbe3ac4533242f65de59c4a526d9b552b9
Varun Mangwani <vpm.media.business@gmail.com>
2026-02-16 10:07:54 +0530

    Varun-Mangwani
    STRUCTURE IN C PRACTICE AND PROJECT QUESTIONS

---
+++ book-struct.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct book
+{
+    int book_id;
+    char name[50];
+    char author[50];
+    int qty;
+    int price;
+    
+    
+
+};
+int main()
+{
+    book *b;
+    int size;
+    printf("\nEnter The Storage To Do: ");
+    b = malloc();
+    return 0;
+}
